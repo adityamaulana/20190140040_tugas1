b3497ca13f878ed25f905b422398036166c5bbe4
adityamaulana <aditmau26@gmail.com>
2023-04-10 10:47:57 +0700

    Pertemuan 6

---
+++ Pertemuan6KelasC.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa
+{
+public :
+    string Name;
+    string NIM;
+
+public : void cetak ()
+{
+    cout << "NIM = " << NIM;
+    cout << "\nName = " << Name;
+}   
+};
+
+class Matakuliah 
+{
+    private :
+        string Code;
+        string StudentName;
+
+        int SKS;
+
+    public :
+        void input()
+        {
+            cout << "Insert Your Lecture Codes Here : ";
+            cout <<
+        }  
+};
